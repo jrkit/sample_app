b5d259507cd40551cc5f839ab21ac39e49986187
kenta <your.email@example.com>
2014-02-09 22:49:06 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+    # ページごとの完全なタイトルを返す
+    def full_title(page_title)
+        base_title =  "Ruby on Rails Tutorial Sample App"
+        if page_title.empty?
+            base_title
+        else
+            "#{base_title} | #{page_title}"
+        end
+    end
+
+    def getRandomString(len)
+        Rails.logger.debug("aaaaaaa----test")
+        @str = ('a'..'z').to_a.shuffle[0..len].join
+        logger.debug(@str)
+        return @str
+    end
 end
